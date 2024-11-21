c1811ae706b94b6548541eef2a5ef8618fe58d36
ghaghrachand <103112848+ghaghrachand@users.noreply.github.com>
2024-11-21 13:52:24 +0000

    container config done

---
+++ 01_basics/01-variables.js
@@ -0,0 +1,10 @@
+const accountId = 1236
+let accountEmail = "abc@gmail.com"
+var city = "Nashik"
+
+city = "Pune"
+accountEmail = "xyz@gmail.com"
+console.table([accountEmail,city]);
+/*
+Prefer not to use var, insted always use let
+*/
