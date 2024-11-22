8eecfc7d4fa840e70118d312e4d21fcde53b68d0
ghaghrachand <103112848+ghaghrachand@users.noreply.github.com>
2024-11-22 17:59:08 +0000

    conversion operations

--- 01_basics/03-conversionOperations.js
+++ 01_basics/03-conversionOperations.js
@@ -1,8 +1,25 @@
 let age = 27
 
-console.log(typeof(age));
+// console.log(typeof(age));
 let valueInNumber = String(age)
-console.log(valueInNumber);
-console.log(typeof(valueInNumber)) 
+// console.log(valueInNumber);
+// console.log(typeof(valueInNumber)) 
 
 //like wise we can convert all the data types but JS is bit confusing in converting the values so all the cross chk the values whether it's boolean,number,string,undefined or anything
+
+//******* operations ********/ //
+
+let value = 3
+let negativeValue = -value
+// console.log(negativeValue)
+
+let str1 = "Hello"
+let str2 = " chand"
+// console.log(str1 + str2)
+// console.log(1+3+"1")
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter=100
+console.log(gameCounter++);
