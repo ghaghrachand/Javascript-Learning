42ce1c77d6ef082a1758fb980a8a8a7730b6e9ac
ghaghrachand <103112848+ghaghrachand@users.noreply.github.com>
2024-11-22 13:19:18 +0000

    dataconversion

---
+++ 01_basics/03-conversionOperations.js
@@ -0,0 +1,8 @@
+let age = 27
+
+console.log(typeof(age));
+let valueInNumber = String(age)
+console.log(valueInNumber);
+console.log(typeof(valueInNumber)) 
+
+//like wise we can convert all the data types but JS is bit confusing in converting the values so all the cross chk the values whether it's boolean,number,string,undefined or anything
