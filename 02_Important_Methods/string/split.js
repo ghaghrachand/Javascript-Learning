a35a6ca3a865b650341c7e1153ec21d8b3b2255b
ghaghrachand <103112848+ghaghrachand@users.noreply.github.com>
2024-11-24 12:39:47 +0000

    split in string

---
+++ 02_Important_Methods/string/split.js
@@ -0,0 +1,20 @@
+// Length
+let schoolName = "Sandip University Nashik"
+// console.log(schoolName.length);
+
+//indexOf(we can give starting index number to find,case sensitive)
+// console.log(schoolName.indexOf('Nashik',3));
+
+// search(it is case sensitive if write it differently U & u it gives -1 as a ans,starting number is not possible to give it starts with 0 only)
+// console.log(schoolName.search('University'));
+
+// startswith(case sensitive,can give index as well)
+// console.log(schoolName.startsWith('University',7))
+
+// console.log(schoolName.endsWith('Nashik'));
+// console.log(schoolName.match('sity'));
+
+
+//***************STRING SPLIT METHOD*************//(converts the string to array)
+const name ="sgdv svdgv uhswi wedoiw wsdrwu keiidm niwjdn wijd"
+console.log(name.split(" "));
